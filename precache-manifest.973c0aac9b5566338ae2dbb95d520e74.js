61644255c6e8d805575366608e5f989013ee2350
ronald dogomeo <ronald.dogomeo@upteamco.com>
2020-04-25 19:03:40 +0800

    Updates

---
+++ precache-manifest.973c0aac9b5566338ae2dbb95d520e74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c10381c661b3c9f6bf43f58bc5132f9",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "9b1499530945aff2f53c",
+    "url": "/react-todo-app/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "39d88aa3be9155a0e736",
+    "url": "/react-todo-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9b1499530945aff2f53c",
+    "url": "/react-todo-app/static/js/2.72cf3764.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-todo-app/static/js/2.72cf3764.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39d88aa3be9155a0e736",
+    "url": "/react-todo-app/static/js/main.70734bf8.chunk.js"
+  },
+  {
+    "revision": "4abf35997a16638d26dd",
+    "url": "/react-todo-app/static/js/runtime-main.d963aa5a.js"
+  }
+]);
